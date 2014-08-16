1d056edfbf9d588413c52563404dfe8d5b86e3ba
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-16 17:54:58 +0200

    Initializer create and convert directory to hash[filename]=file_body

--- database.rb
+++ database.rb
@@ -1,3 +1,18 @@
 class Database
+  include Enumerable
 
+  def initialize
+    @database = Hash.new
+    Dir.entries('testdir').sort[2..-1].each { |key| @database[key.to_sym] = File.read("testdir/#{key}")}
+  end
+
+  def keys
+    @database.keys
+  end
+ 
 end
+
+
+db = Database.new
+
+p db.keys
