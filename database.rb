46de9cdd1feed13777a1733e58a45cc2bc3414b8
Grzegorz Suder <gregor.afc@gmail.com>
2014-08-16 21:53:58 +0200

    add method find for shortcuts

--- database.rb
+++ database.rb
@@ -15,12 +15,27 @@ class Database
     @database[file_name.to_sym]
   end
 
-  def [](file_name)
-    if @database[file_name.to_sym] == nil
-      return "Non existing file"
-    else
-      @database[file_name.to_sym]
+  def find(file_name)
+    file = []
+    keys.each do |key|
+      if key.to_s.include? file_name
+        file << key
+      end
     end
+    file 
+  end
+
+  def [](file_name)
+
+      files = find(file_name)
+
+      if files.size == 0
+        return "Non existing file"
+      elsif files.size == 1
+        return @database[files[0]]
+      else
+        return "Pasujące pliki #{files}"
+      end
   end
 
   def set(file_name, content)
@@ -38,10 +53,17 @@ end
 
 db = Database.new("testdir")
 
-p db.keys
 p db["1.txt"]
 #db["secret.txt"] = "Facebook password"
 #p db["secret.txt"]
-p db["4.txt"]
+#p db["4.txt"]
+p db["test"]
+
+
+
+
+
+
+
 
 
